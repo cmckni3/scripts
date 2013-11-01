9fa94f5bad3d1e33417d64628f34418d0441786f
Chris McKnight <cmckni3@gmail.com>
2013-11-01 15:32:41 -0500

    Add rmagick example

---
+++ thumbnail.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'RMagick'
+image = Magick::ImageList.new('image.jpg').first
+thumb = image.resize_to_fit(300, 300)
+thumb.write File.expand_path(['~/Downloads/thumbnail_test', 'png'].join('.'))
+
+
+image = Magick::ImageList.new('file.pdf').first
+thumb = image.resize_to_fit(300, 300)
+thumb.write(File.expand_path(['~/Downloads/thumbnail_test2', 'png'].join('.')))

04e00f8da6d4a3cad14ad8e8c436d518726a46e6
Chris McKnight <cmckni3@gmail.com>
2017-03-08 15:30:37 -0600

    feat(git-status): Add status of stashes

--- git-status.rb
+++ git-status.rb
@@ -10,6 +10,7 @@ Dir.glob(File.join('**', '.git')).each do |dir|
   Dir.chdir(dir_list.join('/'))
   status   = `git status`
   remotes  = `git remote -v`
+  stashes  = (`git stash list`).split("\n")
   statuses = {
     'Untracked' => "\e[31m\[Untracked\]\e[0m",
     'Changes not staged for commit' => "\e[31m\[Modified\]\e[0m",
@@ -21,7 +22,7 @@ Dir.glob(File.join('**', '.git')).each do |dir|
   statuses.each do |key, value|
     found_statuses << value if status.include?(key)
   end
-  if found_statuses.any? || remotes.length.zero?
+  if found_statuses.any? || remotes.length.zero? || stashes.length > 0
     puts File.expand_path('.')
   end
   if found_statuses.any?
@@ -29,6 +30,9 @@ Dir.glob(File.join('**', '.git')).each do |dir|
   end
   if remotes.length.zero?
     puts "  \e[1;33mNo remote found\e[0m"
+  end
+  unless stashes.length.zero?
+    puts "  \e[36m #{stashes.length} stashes found\e[0m"
   end
   Dir.chdir(cwd)
 end
